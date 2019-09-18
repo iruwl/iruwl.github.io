4faae6902a8ae77c69ed93c7e69a2dae6e7584d1
irul <irul.sylva@gmail.com>
2019-09-18 12:05:37 +0700

    Merubah tampilan author pada sidebar

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -14,26 +14,20 @@ type Props = {
 
 const Author = ({ author, isIndex }: Props) => (
   <div className={styles['author']}>
-    <Link to="/">
+    {<Link to="/">
       <img
         src={withPrefix(author.photo)}
         className={styles['author__photo']}
-        width="75"
-        height="75"
         alt={author.name}
       />
     </Link>
+    }
 
-    { isIndex === true ? (
-      <h1 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
-      </h1>
-    ) : (
-      <h2 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
-      </h2>
-    )}
-    <p className={styles['author__subtitle']}>{author.bio}</p>
+    <h1 className={styles['author__title']}>
+      <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+      &nbsp;
+      <span style={{'font-weight': 'normal', 'color': 'grey'}}>&lt;!--{author.bio}--&gt;</span>
+    </h1>
   </div>
 );
 
