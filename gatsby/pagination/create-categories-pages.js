be3773548cab0212d68d45d00171c4ef81cbcb52
irul <irul.sylva@gmail.com>
2019-09-18 12:29:39 +0700

    Menyesuaikan path page menjadi halaman

--- gatsby/pagination/create-categories-pages.js
+++ gatsby/pagination/create-categories-pages.js
@@ -27,15 +27,15 @@ module.exports = async (graphql, actions) => {
 
     for (let i = 0; i < numPages; i += 1) {
       createPage({
-        path: i === 0 ? categorySlug : `${categorySlug}/page/${i}`,
+        path: i === 0 ? categorySlug : `${categorySlug}/halaman/${i}`,
         component: path.resolve('./src/templates/category-template.js'),
         context: {
           category: category.fieldValue,
           currentPage: i,
           postsLimit: postsPerPage,
           postsOffset: i * postsPerPage,
-          prevPagePath: i <= 1 ? categorySlug : `${categorySlug}/page/${i - 1}`,
-          nextPagePath: `${categorySlug}/page/${i + 1}`,
+          prevPagePath: i <= 1 ? categorySlug : `${categorySlug}/halaman/${i - 1}`,
+          nextPagePath: `${categorySlug}/halaman/${i + 1}`,
           hasPrevPage: i !== 0,
           hasNextPage: i !== numPages - 1
         }
