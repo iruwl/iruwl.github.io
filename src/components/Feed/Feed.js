de6d4dac1693357384146df0f2812cbfb802bf86
irul <irul.sylva@gmail.com>
2019-09-18 11:50:14 +0700

    Merubah format tanggal menjadi format indonesia

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import { Link } from 'gatsby';
 import type { Edges } from '../../types';
 import styles from './Feed.module.scss';
+import 'moment/locale/id';
 
 type Props = {
   edges: Edges
@@ -14,8 +15,8 @@ const Feed = ({ edges }: Props) => (
     {edges.map((edge) => (
       <div className={styles['feed__item']} key={edge.node.fields.slug}>
         <div className={styles['feed__item-meta']}>
-          <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
-            {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
+          <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('D MMMM YYYY')}>
+            <span style={{'color':'#a6a6a6'}}>{moment(edge.node.frontmatter.date).format('D MMMM YYYY')}</span>
           </time>
           <span className={styles['feed__item-meta-divider']} />
           <span className={styles['feed__item-meta-category']}>
@@ -26,7 +27,7 @@ const Feed = ({ edges }: Props) => (
           <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
         </h2>
         <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
-        <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Read</Link>
+        <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Klik di sini untuk lanjut membaca...</Link>
       </div>
     ))}
   </div>
