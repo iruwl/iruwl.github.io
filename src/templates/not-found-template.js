809e186e706b7868be5628479f4248e0cea50a7e
irul <irul.sylva@gmail.com>
2019-09-18 12:01:22 +0700

    Merubah template page not found

--- src/templates/not-found-template.js
+++ src/templates/not-found-template.js
@@ -9,10 +9,10 @@ const NotFoundTemplate = () => {
   const { title, subtitle } = useSiteMetadata();
 
   return (
-    <Layout title={`Not Found - ${title}`} description={subtitle}>
+    <Layout title={`404 - ${title}`} description={subtitle}>
       <Sidebar />
-      <Page title="NOT FOUND">
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Page title="404">
+        <p>Halaman tidak ditemukan.</p>
       </Page>
     </Layout>
   );
