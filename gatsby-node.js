5ed4435cf7ea18bb4cf515fe247c25acac1479dc
irul <irul.sylva@gmail.com>
2019-09-18 14:44:21 +0700

    Menambahkan script copy README.md ke direktori public

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,3 +2,8 @@
 
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
+
+var fs = require('fs-extra')
+var path = require('path')
+console.log('Copy README.md')
+fs.copySync(path.join(__dirname, '/README.md'), path.join(__dirname, '/public/README.md'))
